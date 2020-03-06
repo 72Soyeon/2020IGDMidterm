dff4ed2f965bc41c4ef5e8da4b79d4f6f5f017fa
So Yeon Kim <syk524@nyu.edu>
2020-03-06 14:06:05 -0500

    7th

---
+++ Assets/Scripts/raycastPlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycastPlayer : MonoBehaviour
+{
+
+    private RaycastHit myHit;
+    public GameObject player;
+    
+
+    public float castingDistance = 2f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+    Ray myRay = new Ray(transform.position, transform.forward);
+    Debug.DrawRay(myRay.origin, myRay.direction *castingDistance, Color.blue);
+
+    if (Physics.Raycast(myRay, out myHit, castingDistance))
+    {
+        if (myHit.transform.gameObject.tag == "memory1")
+        {
+            //Debug.Log ("memory 1 hit");
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("memory 1 hit");
+                myHit.transform.gameObject.GetComponent<memoryControl>().TriggerMemory();
+                myHit.transform.gameObject.GetComponent<BoxCollider>().enabled = false;
+            }
+        }
+
+        if (myHit.transform.gameObject.tag == "memory1 NYC")
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+               player.transform.position = new Vector3((float) 3.1, (float) -0.4,(float) 10.5);
+                myHit.transform.gameObject.SetActive(false);
+            }
+        }
+
+    }
+    
+    }
+    
+    
+}
---
+++ Assets/memoryControl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class memoryControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject cursor;
+    public GameObject itemScene;
+    public Text itemText;
+    public string itemDescription;
+    public bool specialSceneOn = false;
+    public GameObject player;
+    public int counter = 0;
+    public 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && counter == 1)
+        {
+            ChangeScene();
+        }
+    }
+
+    public void TriggerMemory()
+    {
+        cursor.SetActive(false);
+        itemScene.SetActive(true);
+        itemText.text = itemDescription;
+        
+        specialSceneOn = true;
+
+        counter += 1;
+        
+    }
+
+    void ChangeScene()
+    {
+        if (specialSceneOn == true)
+        {
+            specialSceneOn = false;
+            counter = 0;
+                
+                itemScene.SetActive(false);
+                cursor.SetActive(true);
+                itemText.text = " ";
+                
+                player.transform.position = new Vector3((float) 82.8, (float) -106.2, (float) -138);
+
+            
+        }
+    }
+}
