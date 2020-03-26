6505105f64b20cca832b237b4ebf3931789fea72
So Yeon Kim <syk524@nyu.edu>
2020-03-26 12:29:53 -0400

    0326 *clamp rotation work in progress

--- Assets/Scripts/firstPersonPhysicsController.cs
+++ Assets/Scripts/firstPersonPhysicsController.cs
@@ -22,6 +22,9 @@ public class firstPersonPhysicsController : MonoBehaviour
 
     public GameObject greeting;
     public GameObject particle;
+
+    private Transform cameraTransform;
+    
     
     
     
@@ -31,7 +34,7 @@ public class firstPersonPhysicsController : MonoBehaviour
         greeting.SetActive(false);        
         particle.SetActive(false);
 
-
+        cameraTransform = Camera.main.transform;
     }
 
     
@@ -43,15 +46,35 @@ public class firstPersonPhysicsController : MonoBehaviour
         //마우스 센시티비티를 고려해서 3정도를 곱해주면 됨.
         mouseX = Input.GetAxis("Mouse X") * 4f ;
         mouseY = Input.GetAxis("Mouse Y") * 4f;
-        
+
         transform.Rotate(0, mouseX, 0);
-        Camera.main.transform.Rotate(-mouseY, 0, 0);
+
         
+        //mouseY = Input.GetAxis("Mouse Y") * 4f;
+        float rotVertical = Input.GetAxis("Mouse Y") * 4f;
+
+        
+        //mouseY = Input.GetAxis("Mouse Y");
+        
+        
+
+       
+        //vertical
+        //Camera.main.transform.Rotate(-mouseY, 0, 0);
+        
+        
+        
+        rotVertical = Mathf.Clamp(rotVertical, 0, 115);
+        cameraTransform.localRotation *= Quaternion.Euler(-rotVertical, 0, 0);
+
+        
+
         forwardBackward = Input.GetAxis("Vertical");
         rightleft = Input.GetAxis("Horizontal");
 
         inputVector = transform.forward * forwardBackward;
         inputVector += transform.right * rightleft;
+        
     }
 
     private void FixedUpdate()
